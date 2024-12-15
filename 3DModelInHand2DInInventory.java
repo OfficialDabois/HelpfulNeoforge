66185951c68139efe5f6f45457804fd792921484
Dabois <68405426+OfficialDabois@users.noreply.github.com>
2024-12-15 16:05:35 +0000

    Create 3DModelInHand2DInInventory.java
    DataGen for a model to be 3d in a players hand but 2d in their inventory

---
+++ 3DModelInHand2DInInventory.java
@@ -0,0 +1,15 @@
+private void ModelInHandFlatInGUI(Item item) {
+  ResourceLocation itemLoc = BuiltInRegistries.ITEM.getKey(item);
+
+  this.getBuilder(item.toString() + "_2d")
+    .parent(new ModelFile.UncheckedModelFile(mcLoc("item/generated")))
+    .texture("layer0", ResourceLocation.fromNamespaceAndPath(itemLoc.getNamespace(), "item/" + itemLoc.getPath() + "_2d"));
+
+  this.getBuilder(item.toString()).customLoader(SeparateTransformsModelBuilder::begin)
+    .base(new ItemModelBuilder(modLoc("item"), existingFileHelper)
+      .parent(new ModelFile.UncheckedModelFile(
+        ResourceLocation.fromNamespaceAndPath(itemLoc.getNamespace(), "item/" + itemLoc.getPath() + "_3d"))))
+    .perspective(ItemDisplayContext.GUI, new ItemModelBuilder(modLoc("item"), existingFileHelper)
+      .parent(new ModelFile.UncheckedModelFile(
+        ResourceLocation.fromNamespaceAndPath(itemLoc.getNamespace(), "item/" + itemLoc.getPath() + "_2d"))));
+}
